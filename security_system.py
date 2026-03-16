f5f0f60c4a1522e3512a3884dfe7b832f5023c96
Sara Alaloul <139572384+sarax22@users.noreply.github.com>
2026-03-16 22:14:52 +0000

    Create security_system.py

---
+++ security_system.py
@@ -0,0 +1,11 @@
+system_armed = True
+motion_detected = True
+door_open = True
+
+# Rules:
+# If the system is armed and motion is detected:
+#     print "Alarm triggered"
+# If the system is armed, motion is detected, and the door is open:
+#     print "Intrusion detected at entry point"
+# Otherwise:
+#     print "System safe"
