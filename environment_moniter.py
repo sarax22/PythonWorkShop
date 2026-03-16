98d87ce21fdf819e0a6673ac5fd320c95e578143
Sara Alaloul <139572384+sarax22@users.noreply.github.com>
2026-03-16 22:14:08 +0000

    Create environment_moniter.py

---
+++ environment_moniter.py
@@ -0,0 +1,9 @@
+temperature = 31
+humidity = 28
+light_level = 180
+
+# Rules:
+# If temperature > 30, print "Fan ON"
+# If humidity < 30, print "Low humidity warning"
+# If light_level < 200, print "Lights ON"
+# If none of the above apply, print "Room conditions normal"
